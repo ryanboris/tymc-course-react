6b9eb41b60e3f997bbeca5c156b0bb7b108c5f11
ryanboris <ryan.boris@gmail.com>
2019-02-17 03:00:10 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Popular from './Popular'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="container">
+      <Popular />
+    </div>
+  )
+}
